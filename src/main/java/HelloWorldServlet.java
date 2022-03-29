fec7ec613e097132e4b5dfeede0ce8e67436c8d4
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-03-29 10:18:42 -0500

    servlet practice

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,17 +1,31 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world/*")
 public class HelloWorldServlet extends HttpServlet {
+    public int counter = 1;
 
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        counter++;
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
-    }
+        String name = request.getParameter("name");
+        String reset = request.getParameter("reset");
+        if (name == null){
+            out.println("<h1>Hello, World!</h1>");
+        } else {
+            out.println("<h1>Hello, " + name + "</h1>");
+        }
+        out.println("<h1>Page views: " + counter + "</h1>");
+        if (reset.equalsIgnoreCase("reset")){
+            counter = 0;
+        }
+        }
 }
